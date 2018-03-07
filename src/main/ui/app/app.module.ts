2c2d5d04163a5effa5dd8649e4c9eb372af084aa
Skeyavodge <jessica.heston@jbhunt.com>
2018-03-07 16:06:35 -0600

    Dummy Service to get Page Sections and adding Mark-up in section cards

--- src/main/ui/app/app.module.ts
+++ src/main/ui/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 import {TabMenuModule} from 'primeng/tabmenu';
 import {MenuItem} from 'primeng/api';
 import {CardModule} from 'primeng/card';
+import { MarkdownModule } from 'angular2-markdown';
 
 
 import { AppComponent } from './app.component';
-import {AppRoutingModule} from './app-routing.module';
-import {HttpClientModule} from "@angular/common/http";
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from "@angular/common/http";
 import { SideNavigationComponent } from './features/side-navigation/side-navigation.component';
 import { ReadViewComponent } from './features/read-view/read-view.component';
 import { EditViewComponent } from './features/edit-view/edit-view.component';
 import { SectionComponent } from './features/section/section.component';
+import {PageService} from "./features/read-view/page.service";
+import {HttpModule} from "@angular/http";
 import {RouterModule} from "@angular/router";
 
 
@@ -28,12 +31,16 @@ import {RouterModule} from "@angular/router";
       BrowserModule,
       BrowserAnimationsModule,
       AppRoutingModule,
+      HttpModule,
       HttpClientModule,
       TabMenuModule,
       RouterModule,
       TabMenuModule,
-      CardModule
+      CardModule,
+      MarkdownModule.forRoot()
   ],
+  schemas: [NO_ERRORS_SCHEMA],
+  providers: [PageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/main/ui/app/features/read-view/page.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { ISection } from '../../entity/section';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/throw';
+
+
+@Injectable()
+export class PageService {
+  private pageUrl = 'http://winx-01331.jbhunt.com:8080/test/page';
+
+  constructor(private http: Http) {}
+
+  getPage(): Observable<ISection[]> {
+    return this.http.get(this.pageUrl)
+        .map((response: Response) => <ISection[]> response.json())
+        .do(data => console.log('All: ' + JSON.stringify(data)))
+        .catch(this.handleError);
+  }
+
+    private handleError(error: Response) {
+        // in a real world app, we may send the server to some remote logging infrastructure
+        // instead of just logging it to the console
+        console.error(error);
+        return Observable.throw(error.json().error || 'Server error');
+    }
+
+}
