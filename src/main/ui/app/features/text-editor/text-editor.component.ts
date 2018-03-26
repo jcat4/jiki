a86b5c0c3c8c6f4e08a67d3bd9a55dffb13e5de7
Skeyavodge <jessica.heston@jbhunt.com>
2018-03-26 17:33:50 -0500

    Adding text editor and hiding titles of secondary sections

---
+++ src/main/ui/app/features/text-editor/text-editor.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-editor',
+  templateUrl: './text-editor.component.html',
+  styleUrls: ['./text-editor.component.scss']
+})
+export class TextEditorComponent implements OnInit {
+
+  color2: string = '#fff431;';
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
